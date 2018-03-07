70ff625a360f51625fe41a9a86fbcebfb5bd01dc
Jonas Segedahl <jonas@Jonass-MacBook-Air.local>
2018-03-07 12:59:19 +0100

    Update

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// Inlcude META DATA
+require "resources/includes/head.php";
+// Include Header
+require "resources/views/header.php";
+// Create function for the subpages
+/*
+-Start
++- läsa inlägg som news,
++-kommentera på inlägg (kräver inlogg)
++-Gilla inlägg
+-about
+-Posts (syns bara för inloggade)
++-man skall kunna posta inlägg
++-
+-profil
++-Rediger information
+++- Namn, Bild, Civilstatus osv. 
+*/
+// Include Footer
+ require 'resources/views/footer.php';
+  ?>
